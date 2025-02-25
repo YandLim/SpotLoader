56d3a932cb5b77fd888e1ad093ce61c0f1e94e3e
Yand_ <anonnniymust@gmail.com>
2025-02-25 10:47:39 +0800

    Done taking the playlist's song or the album's song or a song from link

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ load_dotenv()
 client_id = os.getenv("CLIENT_ID")
 client_secret = os.getenv("CLIENT_SECRET")
 
+
 def get_token():
     auth_string = client_id + ":" + client_secret
     auth_bytes = auth_string.encode("utf-8")
@@ -26,32 +27,66 @@ def get_token():
     token = json_result["access_token"]
     return token
 
+
 def get_auth_header(token):
     return {"Authorization": "Bearer "+ token}
 
-def search_for_artict(token, artist_name):
-    url = "https://api.spotify.com/v1/search"
-    headers = get_auth_header(token)
-    query = f"?q={artist_name}&type=artist&limit=1"
 
-    query_url = url + query
-    result = get(query_url, headers=headers)
-    json_result = json.loads(result.content)["artists"]["items"]
-    if len(json_result) == 0:
-        print("No artist with this name found")
-        return None
-    return json_result[0]
-
-def get_artist_song(token, artist_id):
-    url = f"https://api.spotify.com/v1/artists/{artist_id}/top-tracks"
+def get_album_song(token, album_id):
+    url = f"https://api.spotify.com/v1/albums/{album_id}"
     headers = get_auth_header(token)
     result = get(url, headers=headers)
-    json_result = json.loads(result.content)["tracks"]
+    json_result = json.loads(result.content)
     return json_result
 
-token = get_token()
-artist = search_for_artict(token, "Nadine abigail")
-songs = get_artist_song(token, artist["id"])
 
-for idx, song in enumerate(songs):
-    print(f"{idx + 1}. {song['name']}")
+def get_playlist_song(token, url):
+    url = url
+    headers = get_auth_header(token)
+    result = get(url, headers=headers)
+    json_result = json.loads(result.content)
+    next_items = json_result["next"]
+    return json_result, next_items
+
+
+def get_songs(token, song_id):
+    url = f"https://api.spotify.com/v1/tracks/{song_id}"
+    headers = get_auth_header(token)
+    result = get(url, headers=headers)
+    json_result = json.loads(result.content)
+    return json_result
+
+
+token = get_token()
+user_link = input("Please enter the link to a playlist or song\n=> ")
+
+if user_link.split("/")[3] == "album":
+    playlist_id = user_link.split("/")[4]
+    playlist_songs = get_album_song(token, playlist_id)
+    items = playlist_songs["tracks"]["items"]
+
+    for idx, song in enumerate(items):
+        print(f"{idx + 1}: {song['name']}")
+
+elif user_link.split("/")[3] == "playlist":
+    playlist_id = user_link.split("/")[4]
+    playlist_url = f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks"
+    playlist_songs, next_playlist = get_playlist_song(token, playlist_url)
+    items = playlist_songs["items"]
+
+    if next_playlist:
+        while next_playlist:
+            playlist_songs, next_playlist = get_playlist_song(token, next_playlist)
+            items.extend(playlist_songs["items"])
+            for idx, song in enumerate(items):
+                print(f"{idx + 1}: {song["track"]['name']}")
+    
+    else:
+        for idx, song in enumerate(items):
+            print(f"{idx + 1}: {song["track"]['name']}")
+
+
+elif user_link.split("/")[3] == "track":
+    song_id = user_link.split("/")[4]
+    songs = get_songs(token, song_id)
+    print("Song Name: " + songs["name"])
